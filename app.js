84de0889a4c84a38275a320b0a5a70cd27e28adf
JamesKempDev <jkpuck@gmail.com>
2021-02-09 15:39:53 +0000

    Added static routes

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 // Set up requirements
 const express = require('express');
 const app = express();
-app.use(express.static('public'));
 const data = require('./data.json');
 const routes = require('./routes.js');
 
 // Set the view engine to pug
 app.set('view engine', 'pug');
+
+app.use('/static', express.static('public'));
 
 app.use('/', routes);
 
