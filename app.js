7558b2b4b6660f7c6b094f182210371d31ad46f4
JamesKempDev <jkpuck@gmail.com>
2021-02-10 11:54:18 +0000

    Error handler included

--- app.js
+++ app.js
@@ -7,10 +7,32 @@ const routes = require('./routes.js');
 // Set the view engine to pug
 app.set('view engine', 'pug');
 
-app.use('/static', express.static('public'));
-
+// Set up the routes handler
 app.use('/', routes);
 
+app.use('/static', express.static('public'));
+
+
+// 404 error handler
+app.use((req, res, next) => {
+    res.send("404 error. See console log for more details ");
+    err = new Error("This page cannot be found");
+    err.status=404;
+    console.log(`${err.message} - ${err.status} returned`);
+    next();
+})
+
+// Global error handler
+app.use((err, req, res, next) => {
+    res.send("Server error. See console log for more details ");
+    err.status = '500';
+    err.message = 'Something really bad happened';
+    console.log(`${err.status} - ${err.message}`);
+    next();
+  })
+
+
+// Open port 3000
 app.listen(3000, () => {
     console.log("listening on port 3000")
 });
--- routes.js
+++ routes.js
@@ -2,14 +2,17 @@ const express = require('express');
 const router = express.Router();
 const data = require('./data.json');
 
-router.get('/', (req, res, next) => {
-    console.log('Home page');
-    console.log(data.projects);
-    next();
+router.get('/', (req, res) => {
+    res.render('../views/index');
 })
 
-router.get('/about', (req, res, next) => {
+router.get('/about', (req, res) => {
     res.render('../views/about');
 })
+
+router.get('/project/:id', (req, res) => {
+    res.render('../views/project');
+    console.log(req.params.id);
+} )
 
 module.exports = router;
